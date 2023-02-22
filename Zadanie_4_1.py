fedac4901837cdd6c9b68f6b9d70825e35776924
Alicja Popielarska <popielarska.alicja@gmail.com>
2023-02-22 19:32:44 +0100

    Added documentation

--- Zadanie_4_1.py
+++ Zadanie_4_1.py
@@ -1,6 +1,12 @@
 def is_palindrom(word):
+    """
+        Function checks if a given word is a palindrom,
+        i. e. if read from left to right is the same
+        as from right to left. 
+        Returns True, if is a palindrom.
+        Returns False, if is not.
+    """
     if word==word[::-1]:
         return True
     else:
         return False
-print(is_palindrom("kot"))
